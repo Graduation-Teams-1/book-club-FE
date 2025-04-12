559dfd5e67896668bf838d143807273228289f5d
abdosamiir <barakatabdo82@gmail.com>
2025-04-12 18:27:23 +0200

    Add react-icons dependency and implement profile editing functionality with modal

--- src/app/pages/client/components/MainSection.tsx
+++ src/app/pages/client/components/MainSection.tsx
@@ -1,20 +1,18 @@
-// import {
-//   GooglePlayButton,
-//   AppStoreButton,
-//   ButtonsContainer,
-// } from "react-mobile-app-button";
+import { AiOutlineApple } from "react-icons/ai";
+import { IoLogoGooglePlaystore } from "react-icons/io5";
 
 import person from "~/assets/imgs/pana.png";
 import header from "~/assets/imgs/Header.png";
 import { Button, Image } from "@mantine/core";
+import { Link } from "react-router-dom";
 // import { useNavigate } from "react-router-dom";
 const MainSection = () => {
-  // const APKUrl = "https://play.google.com/store/games?device=windows";
-  // const IOSUrl = "https://www.apple.com/eg-ar/app-store/";
+  const APKUrl = "https://play.google.com/store/games?device=windows";
+  const IOSUrl = "https://www.apple.com/eg-ar/app-store/";
   // const navigate = useNavigate();
   return (
     <>
-      <div className="bg-home flex h-[60rem] items-start justify-center gap-40 bg-contain bg-right-top bg-no-repeat pt-40">
+      <div className="flex h-[60rem] items-start justify-center gap-40 bg-home bg-contain bg-right-top bg-no-repeat pt-40">
         <div className="flex w-1/2 flex-col items-center justify-center gap-14">
           <Image src={header} alt="header" w={600} />
           <p className="w-[85%] text-center text-2xl font-semibold text-[#402905]">
@@ -30,19 +28,14 @@ const MainSection = () => {
           >
             Get Started
           </Button>
-          {/* <ButtonsContainer>
-            <GooglePlayButton
-              url={APKUrl}
-              theme={"light"}
-              className="!border-gray-400 !py-[1.65rem]"
-            />
-
-            <AppStoreButton
-              url={IOSUrl}
-              theme={"light"}
-              className="!border-gray-400 !py-[1.65rem]"
-            />
-          </ButtonsContainer> */}
+          <div className="flex gap-4">
+            <Link to={APKUrl} target="_blank">
+              <IoLogoGooglePlaystore className="h-14 w-14 cursor-pointer" />
+            </Link>
+            <Link to={IOSUrl} target="_blank">
+              <AiOutlineApple className="h-14 w-14 cursor-pointer" />
+            </Link>
+          </div>
         </div>
         <Image src={person} alt="person" className="!w-1/3 !object-contain" />
       </div>
